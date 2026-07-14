abab21355e06b472b5709745bbb25ac9ac48b7cf
at914 <amruttelavane914@gmail.com>
2026-07-14 11:24:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ChatApp from './components/ChatApp'
+import UserDetails from './components/UserDetails'
 
 function App() {
   return (
     <div className="App">
       <ChatApp/>
+      <UserDetails/>
     </div>
   );
 }
-
 export default App;
